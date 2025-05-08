ec0d38f909af4bfc388ef7a4fb3ce2a67f4fd6a5
Beer <bjfeddes@gmail.com>
2025-05-08 15:44:37 +0200

    Update vaarwegDashboard.py

--- vaarwegDashboard.py
+++ vaarwegDashboard.py
@@ -89,7 +89,7 @@ def display_grafiek_gem(df, Xas, vaart, Zas, stack, tabel):
     st.bar_chart(df_totaal, x= Xas, y= 'gem', y_label= ' gem aantal schepen per dag', color= 'dagsoort', stack=stack)
     df_totaal.index = df_totaal.maand if (Xas == 'maand') else df_totaal.seizoen
     df_totaal = df_totaal.drop(columns=['Timestamp','maand','seizoen']) if Xas == ('maand') else df_totaal.drop(columns=['seizoen'])
-    # if tabel=='ja' st.write(df_totaal)
+    st.write(df_totaal)
 
 def pod_kleur(val):
     color = 'green' if int(val) else 'red'
